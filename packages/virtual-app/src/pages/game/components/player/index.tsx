4fbc43fd9ad36e7ec8b0e0145ac90d79349132db
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-09-25 20:53:39 -0300

    Add basic quarks particles

--- packages/virtual-app/src/pages/game/components/player/index.tsx
+++ packages/virtual-app/src/pages/game/components/player/index.tsx
@@ -1,10 +1,10 @@
-import { useRef, useState } from "react";
-import { Group } from "three";
+import { useMemo, useRef, useState } from "react";
+import { AdditiveBlending, Group, MeshBasicMaterial } from "three";
 import { useConnector } from "../../../../lib/connector";
 import { useSubscribe } from "../../../../lib/subscribable";
 
 import { CAMERA_NAMES, useGame } from "../../../../lib/use-game";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import {
   clamp,
   clampLerp,
@@ -13,6 +13,7 @@ import {
   round,
 } from "../../../../lib/math";
 import { lineWidth } from "../road/use-road";
+import * as q from "three.quarks";
 
 /// TODO: fix this asset
 import { Motorcycle } from "../vehicles/motorcycle";
@@ -94,25 +95,73 @@ export const Player = () => {
 
   const [gameOver, setGameOver] = useState(false);
 
+  const scene = useThree((state) => state.scene);
+
+  const [batchSystem] = useMemo(() => {
+    const batchSystem = new q.BatchedParticleRenderer();
+
+    const explossionSystem = new q.ParticleSystem({
+      duration: 1,
+      looping: true,
+      startLife: new q.IntervalValue(0.1, 0.2),
+      startSpeed: new q.ConstantValue(1),
+      startSize: new q.IntervalValue(1, 5),
+      startColor: new q.ConstantColor(new q.Vector4(1, 1, 1, 1)),
+      material: new MeshBasicMaterial({
+        color: 0xff0000,
+        blending: AdditiveBlending,
+      }),
+      shape: new q.ConeEmitter({}),
+
+      emissionBursts: [
+        {
+          time: 2,
+          count: new q.ConstantValue(100),
+          cycle: 1,
+          interval: 0.01,
+          probability: 1,
+        },
+      ],
+      renderOrder: 2,
+      startTileIndex: new q.ConstantValue(91),
+      uTileCount: 10,
+      vTileCount: 10,
+      renderMode: q.RenderMode.Mesh,
+    });
+
+    batchSystem.addSystem(explossionSystem);
+
+    scene.add(batchSystem);
+    scene.add(explossionSystem.emitter);
+
+    return [batchSystem, explossionSystem] as const;
+  }, [scene]);
+
+  useFrame((_, delta) => {
+    batchSystem.update(delta);
+  });
+
   if (gameOver) return null;
 
   return (
-    <group ref={containerRef}>
-      <PerspectiveCamera
-        makeDefault={activeCamera}
-        fov={30}
-        position={[0, 10, 20]}
-        rotation={[degToRad(-20), 0, 0]}
-      />
-      <group>
-        <Motorcycle
-          onIntersectionEnter={() => {
-            useConnector.getState().onLose.runCallbacks();
-            setGameOver(true);
-          }}
-          ref={carRef}
+    <>
+      <group ref={containerRef}>
+        <PerspectiveCamera
+          makeDefault={activeCamera}
+          fov={30}
+          position={[0, 10, 20]}
+          rotation={[degToRad(-20), 0, 0]}
         />
+        <group>
+          <Motorcycle
+            onIntersectionEnter={() => {
+              useConnector.getState().onLose.runCallbacks();
+              setGameOver(true);
+            }}
+            ref={carRef}
+          />
+        </group>
       </group>
-    </group>
+    </>
   );
 };
